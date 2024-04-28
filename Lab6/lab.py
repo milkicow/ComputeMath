784a1d70b1aa941c57a7806426cb99a683dedc79
milkicow <kozlov.aleksandr@phystech.edu>
2024-04-28 22:07:41 +0300

    Adjustments for convenient use

--- Lab6/lab.py
+++ Lab6/lab.py
@@ -6,33 +6,32 @@ import scipy as sp
 sigma = 10
 r = 28
 b = 8 / 3
-grid_size = 100000
 
-def f_x(y):
+def f_x(x, y):
     return -sigma * (y[0] - y[1])
 
-def f_y(y):
+def f_y(x, y):
     return -y[0] * y[2] + r * y[0] - y[1]
 
-def f_z(y):
+def f_z(x, y):
     return y[0] * y[1] - b * y[2]
 
-def calc_k_one_arg(f, arg_i: int, y: list, h: float) -> list: # classic 4th rang
+def calc_k_one_arg(f, arg_i: int, x: float, y: list, h: float) -> list: # classic 4th rang
     k = []
-    k.append(f(y))
-    k.append(f(y + [h / 2 * k[0] for i in y]))
-    k.append(f(y + [h / 2 * k[1] for i in y]))
-    k.append(f(y + [h * k[2] for i in y]))
+    k.append(f(x, y))
+    k.append(f(x, y + [h / 2 * k[0] for i in y]))
+    k.append(f(x, y + [h / 2 * k[1] for i in y]))
+    k.append(f(x, y + [h * k[2] for i in y]))
     return k
 
-def calc_k(F: list, y: list, h: float) -> list[list]:
+def calc_k(F: list, x: float, y: list, h: float) -> list[list]:
     k = []
     for i in range(len(y)):
-        k.append(calc_k_one_arg(F[i], i, y, h))
+        k.append(calc_k_one_arg(F[i], i, x, y, h))
     return k
 
-def runge_kutta_step(F: list, y: list, h: float):
-    k = calc_k(F, y, h)
+def runge_kutta_step(F: list, x: float, y: list, h: float):
+    k = calc_k(F, x, y, h)
     y_next = []
 
     b = [1/6, 2/6, 2/6, 1/6]
@@ -62,72 +61,67 @@ def prepare_arrays_expl(points: list[list]):
 
     return x, y, z
 
-def expl_runge_kutta(y_0: list, t_final: float):
-    h = t_final / grid_size
-    F = [f_x, f_y, f_z]
-    
+def expl_runge_kutta(F: list, t_0: float, y_0: list, t_final: float, grid_size: int):
+    h = (t_final - t_0) / grid_size
+
     y_list = [y_0]
 
-    t = 0
     y_next = y_0
-    while (t < t_final):
+    while (t_0 < t_final):
         y = y_next
 
-        y_next = runge_kutta_step(F, y, h)
-        t += h
+        y_next = runge_kutta_step(F, t_0, y, h)
+        t_0 += h
         y_list.append(y_next)
 
     return y_list
 
-def adams(y_0: list, t_final: float):
+def adams(F: list, y_0: list, t_final: float, grid_size: int):
     h = t_final / grid_size
-    F = [f_x, f_y, f_z]
-
     y_list = [y_0]
 
     y_prev = y_0
     y = y_0
     y_next = y_0
-    t = 0
+    t = h
     while (t < t_final):
         y_prev = y
         y = y_next
 
         y_next = [0, 0, 0]
         for i in range(len(y)):
-            y_next[i] = y[i] + h * (3 / 2 * F[i](y) - 1 / 2 * F[i](y_prev))
+            y_next[i] = y[i] + h * (3 / 2 * F[i](t, y) - 1 / 2 * F[i](t, y_prev))
     
         y_list.append(y_next)
         t += h
     return y_list
 
-def backward_diff(y_0: list, t_final: float):
+def backward_diff(F: list, y_0: list, t_final: float, grid_size: int):
     h = t_final / grid_size
-    F = [f_x, f_y, f_z]
-
     y_list = [y_0]
 
     y_prev = y_0
     y = y_0
     y_next = y_0
-    t = 0
+    t = h
     while (t < t_final):
         y_prev = y
         y = y_next
         
         y_next = [0, 0, 0]
         for i in range(len(y)):
-            y_next[i] = y_prev[i] + 2 * h * F[i](y)
+            y_next[i] = y_prev[i] + 2 * h * F[i](t, y)
 
         y_list.append(y_next)
         t += 0.8
     return y_list
 
 def explicit_methods():
-    y_list = expl_runge_kutta([1, 1, 1], 50)
-    y_list1 = adams([1, 1, 1], 50)
-    y_list2 = backward_diff([1, 1, 1], 50)
-
+    grid_size = 100000
+    F = [f_x, f_y, f_z]
+    y_list = expl_runge_kutta(F, 0, [1, 1, 1], 50, grid_size)
+    y_list1 = adams(F, [1, 1, 1], 50, grid_size)
+    y_list2 = backward_diff(F, [1, 1, 1], 50, grid_size)
     x0, y0, z0 = prepare_arrays_expl(y_list)
     x1, y1, z1 = prepare_arrays_expl(y_list1)
     x2, y2, z2 = prepare_arrays_expl(y_list2)
@@ -155,7 +149,7 @@ def F(y):
          betta * y[0] * (1 - y[1] / C) * (1 + (y[1] / fi0) * (y[1] / fi0)) - k2 * y[1]
     ])
 
-def impl_runge_kutta(f, y_0: list, t_final: float):
+def impl_runge_kutta(f, y_0: list, t_final: float, grid_size: int):
     h = t_final / grid_size
 
     y_list = [y_0]
@@ -178,7 +172,7 @@ def impl_runge_kutta(f, y_0: list, t_final: float):
     return y_list, t_list
 
 
-def rosenbrock(f, y_0: list, t_final: float):
+def rosenbrock(f, y_0: list, t_final: float, grid_size: int):
     h = t_final / grid_size
     y_h = h
 
@@ -204,7 +198,7 @@ def rosenbrock(f, y_0: list, t_final: float):
         t += h
     return y_list, t_list
 
-def nordsik(f, y_0: list, t_final: float):
+def nordsik(f, y_0: list, t_final: float, grid_size: int):
     h = t_final / grid_size
     ord = 4
 
@@ -265,13 +259,14 @@ def do_plot_impl(tetta0, fi0, tetta1, fi1, tetta2, fi2, t):
     plt.show()
 
 def implicit_methods():
-    y_list, t_list = rosenbrock(F, np.array([tetta0, fi0]), 10)
+    grid_size = 100000
+    y_list, t_list = rosenbrock(F, np.array([tetta0, fi0]), 10, grid_size)
     tetta1_, fi1_ = prepare_arrays_impl(y_list)
 
-    y_list, t_list = impl_runge_kutta(F, np.array([tetta0, fi0]), 10)
+    y_list, t_list = impl_runge_kutta(F, np.array([tetta0, fi0]), 10, grid_size)
     tetta0_, fi0_ = prepare_arrays_impl(y_list)
 
-    y_list, t_list = nordsik(F, np.array([tetta0, fi0]), 10)
+    y_list, t_list = nordsik(F, np.array([tetta0, fi0]), 10, grid_size)
     tetta2_, fi2_ = prepare_arrays_impl(y_list)
     do_plot_impl(tetta0_, fi0_, tetta1_, fi1_, tetta2_, fi2_, t_list)
 
